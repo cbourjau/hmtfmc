46dade7bf63d5afacf5b5a4b71a448fcde7e22ce
Christian Bourjau <c.bourjau@posteo.de>
2015-07-17 16:30:40 +0200

    unified local and lite

--- runProof.C
+++ runProof.C
@@ -3,15 +3,13 @@
 #include "TROOT.h"
 #include "TProof.h"
 #include "TChain.h"
+#include "TObject.h"
 
 #include "AliAnalysisManager.h"
 #include "AliESDInputHandler.h"
 #endif
 
-void runProof(const char * incollection = "./input/input_files.dat", const char * outfile = "MC_MultEst.root")
-{
-  TProof::Open("lite://");
-
+void loadLibs(const TString extralibs){
   // for running with root only
   gSystem->Load("libTree.so");
   gSystem->Load("libGeom.so");
@@ -20,12 +18,15 @@ void runProof(const char * incollection = "./input/input_files.dat", const char 
   gSystem->Load("libESD.so");
   gSystem->Load("libAOD.so"); 
 
-  // load analysis framework
-  gSystem->Load("libANALYSIS");
-  gSystem->Load("libANALYSISalice");
-  gSystem->Load("$ALICE_ROOT/lib/libpythia6_4_25.so");
-  gSystem->Load("$ALICE_ROOT/lib/libpythia6.so");
+  // load extra analysis libs
+  TIter it(extralibs.Tokenize(":"));
+  TObjString *lib = 0;
+  while ((lib = dynamic_cast<TObjString *>(it()))){
+    gSystem->Load(lib->String());
+  }
+}
 
+TChain* makeChain(const char* incollection) {
   TChain * chain = new TChain ("TE");
   if (incollection == 0) {
     // if no collection is given, check for a file in workdir
@@ -37,50 +38,87 @@ void runProof(const char * incollection = "./input/input_files.dat", const char 
   //   while(coll->Next()){
   //     chain->Add(TString("alien://")+coll->GetLFN());
   //   }
-  {
-    ifstream file_collect(incollection);
-    TString line;
-    while (line.ReadLine(file_collect) ) {
-      chain->Add(line.Data());
+  ifstream file_collect(incollection);
+  TString line;
+  while (line.ReadLine(file_collect) ) {
+    chain->Add(line.Data());
+  }
+  return chain;
+}
+
+void loadAnalysisFiles(const TString files, TString runmode ) {
+  // load files from colon (:) separated string
+  // first file in string is loaded first (OBS: dependencies)
+  // The function does the right thing regardless if local, lite or pod
+  // Do not use ++ in the string to avoid unnecessary recompiles on nodes
+  
+  TIter it(files.Tokenize(":"));
+  TObjString *lib = 0;
+  while ((lib = dynamic_cast<TObjString *>(it()))){
+    Int_t status;
+    if (runmode.BeginsWith("local")){
+      status = gROOT->LoadMacro(lib->String());
+    }
+    else {
+      status = gProof->Load(lib->String());
+    }
+    if (status !=0){
+	std::cout << "Error loading " << lib->String() << std::endl;
     }
   }
-  //chain->GetListOfFiles()->Print();
-  //
-  // for includes use either global setting in $HOME/.rootrc
-  // ACLiC.IncludePaths: -I$(ALICE_ROOT)/include
-  // or in each macro
+}
+
+void runProof(const TString runmode_str  = "lite",
+	      Int_t max_events = 1000,
+	      const char * incollection = "./input/input_files.dat",
+	      const char * analysisName = "hmtf_mc_mult",
+	      const char * aliceExtraLibs=("libANALYSIS:"
+					   "libANALYSISalice:"
+					   "libpythia6_4_25"),
+	      const char * analysisFiles=("MultiplicityEstimators.cxx+g:"
+					  "AliAnalysisTaskHMTFMCMultEst.cxx+g"),
+	      const TString addAnalysisFiles=("AddTaskHMTFMCMultEst.C"))
+{
+  if(!(runmode_str.BeginsWith("local") ||
+       runmode_str.BeginsWith("lite") ||
+       runmode_str.BeginsWith("pod"))){
+    std::cout << "invalid runmode given" << std::endl;
+    return;
+  }
+  if (runmode_str.BeginsWith("lite")){
+    TProof::Open("lite://");
+  }
+  
+  loadLibs(aliceExtraLibs);
+  TChain *chain = makeChain(incollection);
+
   gSystem->AddIncludePath("-I$ALICE_ROOT/include");
 
-  // Create the analysis manager
-  AliAnalysisManager *mgr = new AliAnalysisManager("HMTFMCMultEst");
+  // Create  and setup the analysis manager
+  AliAnalysisManager *mgr = new AliAnalysisManager(analysisName);
+  mgr->SetCommonFileName(TString(analysisName) + TString(".root"));
 
+  // Set Handlers
   AliVEventHandler* esdH = new AliESDInputHandler;
   mgr->SetInputEventHandler(esdH);
-  mgr->SetCommonFileName(outfile);
-  
-  // Enable MC event handler
   AliMCEventHandler* handler = new AliMCEventHandler;
   handler->SetReadTR(kFALSE);
   mgr->SetMCtruthEventHandler(handler);
 
-  // Create containers for input/output before adding tasks
-  //AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
+  loadAnalysisFiles(analysisFiles, runmode_str);
 
-  // Create and add tasks
-  gProof->Load("MultiplicityEstimators.cxx+g");
-  gProof->Load("AliAnalysisTaskHMTFMCMultEst.cxx+g");
-  gROOT->LoadMacro("AddTaskHMTFMCMultEst.C");
-  AliAnalysisTask *task1 = AddTaskHMTFMCMultEst();
-
-  mgr->AddTask(task1);
+  // Add tasks
+  TIter it(addAnalysisFiles.Tokenize(":"));
+  TObjString *adder = 0;
+  while ((adder = dynamic_cast<TObjString *>(it()))){
+    mgr->AddTask(reinterpret_cast<AliAnalysisTask*>(gROOT->Macro(adder->String())));    
+  }
 
   // Enable debug printouts
   mgr->SetDebugLevel(0);
 
   if (!mgr->InitAnalysis()) return;
-
   mgr->PrintStatus();
-
-  mgr->StartAnalysis("proof", chain);
-  return;
+  if (runmode_str.BeginsWith("lite")) mgr->StartAnalysis("proof", chain, max_events);
+  else if (runmode_str.BeginsWith("local")) mgr->StartAnalysis("local", chain, max_events);
 }
