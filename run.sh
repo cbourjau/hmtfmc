0bfb4f1d39d07bd52abb2ed01b71e03ce3727235
Christian Bourjau <c.bourjau@posteo.de>
2015-07-23 17:12:26 +0200

    updated run.sh script

--- run.sh
+++ run.sh
@@ -1,38 +1,52 @@
-#!/bin/sh
-wks="&workers=4"
-rv="&pattern=galice.root&recursive"
+#!/bin/bash
+incoll=""
 case x$1 in
     xlocal)
 	name="local"
-	wks=
-	url="local://${PWD}/input" ;;    
+	case x$2 in
+	    xpythia)
+		incoll="${PWD}/pythia/input_files.dat"
+		;;
+	    xphojet)
+		incoll="${PWD}/phojet/input_files.dat"
+		;;
+	esac
+	;;
     xlite)
 	name="lite"
-	url="lite://${PWD}/input" ;;
-    xproof|xcaf)
-	name="caf"
-	# rv="&root=v5-34-08"
-	wks= #"&workers=10"
-	rv="&aliphysics=vAN-20150323&root=v5-34-08&aliroot=v5-06-19&plugin"
-	url="proof://alice-caf.cern.ch//default/ilakomov/TestDownScale1" ;;
-    xalien|xgrid)
-	name="alien"
-	rv="&aliphysics=last,regular&pattern=*/galice.root&files=../inputs"
-	dir="/alice/cern.ch/user/p/pwgpp_mc/2015/17_Week/TestMultiplicity/Test2/Test_1M_events_iter1"
-	url="alien://${dir}"
+	case x$2 in
+	    xpythia)
+		incoll="${PWD}/pythia/input_files.dat"
+		;;
+	    xphojet)
+		incoll="${PWD}/phojet/input_files.dat"
+		;;
+	esac
+	;;
+    xpod)
+	name="pod"
+	case x$2 in
+	    xpythia)
+		incoll="pythia"
+		;;
+	    xphojet)
+		incoll="phojet"
+		;;
+	esac
 	;;
     x--help)
-	echo "Usage: $0 [local|lite|proof]" ; exit 0 ;; 
+	echo "Usage: $0 [local|lite|proof]" ; exit 0
+	;; 
     *)
-	echo "No or Unknown type ($1) specified" ; exit 1 ;;
+	echo "No or Unknown type ($1) specified" ; exit 1
+	;;
 esac
 shift
 
-url="${url}?mc${rv}${wks}#TE"
-
-rm -rf $name
-set -x
-runTrain --class=HMTFMCMultEstTrain --name=$name --type=ESD --url="$url" $@ 
+#rm -rf $name
+#set -x
+root -l -x "runProof.C(\"${name}\", -1, 0, \"${incoll}\")"
+#runTrain --class=HMTFMCMultEstTrain --name=$name --type=ESD --url="$url" $@ 
 
 #
 # EOF
