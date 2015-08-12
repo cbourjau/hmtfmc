bb29ef5908cea59d275a454593a68ac3c8ecfa04
Christian Bourjau <c.bourjau@posteo.de>
2015-08-12 14:51:19 +0200

    download script for pythia files

---
+++ pythia/dl.py
@@ -0,0 +1,23 @@
+import os
+
+basepath = "/alice/cern.ch/user/p/pwgpp_mc/2015/17_Week/TestMultiplicity/Test2/Test_1M_events_iter1/"
+remotefiles = os.popen("alien_find {} galice.root".format(basepath))
+print remotefiles
+max_files = 1000
+
+for idx, remotefile in enumerate(remotefiles):
+    if idx > max_files:
+        break
+    # break at first empty line
+    remotepath = "alien://" + remotefile.strip().replace("galice.root", "")
+    local_folder = remotepath.split('/')[-2]
+    if not remotefile:
+        break
+    try:
+        os.mkdir(remotepath.split('/')[-2])
+    except OSError:
+        print "Not re-downloading remotefile. (Folder already exists)"
+        continue
+    print os.popen("alien_cp -m -s alien://{} ./{}".format(remotepath+"*.root", local_folder)).read()
+
+os.popen("find ${PWD}/*/galice.root > input_files.dat")
